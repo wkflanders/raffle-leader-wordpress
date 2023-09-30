ec1a118f8bdcfcb6325518e2bb8678d90bfb5631
wkflanders <wflanders@haverford.edu>
2023-09-30 18:04:58 -0400

    plugin architecture setup

--- wp-content/plugins/raffleleader/raffleleader.php
+++ wp-content/plugins/raffleleader/raffleleader.php
@@ -1,2 +1,31 @@
 <?php
 
+/**
+ * Plugin Name: RaffleLeader
+ * Version: 1.0.0
+ * Description: A leader in market lead generation
+ */
+
+if (!defined('ABSPATH')) {
+    die();
+}
+
+if (!class_exists('RaffleLeader')) {
+    class RaffleLeader{
+
+        public function __construct(){
+            define('PLUGIN_PATH', plugin_dir_path(__FILE__));
+            require_once(PLUGIN_PATH . '/vendor/autoload.php');
+        }
+
+        public function initialize(){
+            include_once PLUGIN_PATH . '/includes/utils.php';
+            include_once PLUGIN_PATH . '/includes/settings/settings.php';
+        }
+
+    }
+
+    $raffleLeader = new RaffleLeader;
+
+    $raffleLeader->initialize();
+}
---
+++ wp-content/plugins/raffleleader/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '4ccdadb379db64f0d3b206cadacc024cbd3af104',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '4ccdadb379db64f0d3b206cadacc024cbd3af104',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);

ffefc38bb6ec345dcf74a4b2b324062f3885fa56
wkflanders <wflanders@haverford.edu>
2023-10-05 21:06:33 -0400

    admin simple behavior

--- wp-content/plugins/raffleleader/includes/Base/Enqueue.php
+++ wp-content/plugins/raffleleader/includes/Base/Enqueue.php
@@ -5,15 +5,17 @@
 
 namespace Includes\Base;
 
+use Includes\Base\BaseController;
+
 class Enqueue extends BaseController{
 
     public function register(){
-        add_action( 'admin_enqueu_scripts', array( $this, 'enqueue' ) );
+        add_action( 'admin_enqueue_scripts', array( $this, 'enqueue' ) );
     }
 
     public function enqueue(){
         wp_enqueue_style( 'raffleleader-style', $this->plugin_url . '/assets/styles.css' );
-        wp_enqueue_style( 'raffleleader-script', $this->plugin_url . '/assets/index.js' );
+        wp_enqueue_script( 'raffleleader-script', $this->plugin_url . '/assets/scripts.js' );
                 
     }
 }

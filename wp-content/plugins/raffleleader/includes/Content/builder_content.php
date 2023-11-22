e8126f77c6be717b1e4e9164b9fa131e8e9ee40d
wkflanders <wflanders@haverford.edu>
2023-11-22 13:25:34 -0500

    template fixes

--- wp-content/plugins/raffleleader/includes/Content/builder_content.php
+++ wp-content/plugins/raffleleader/includes/Content/builder_content.php
@@ -19,32 +19,7 @@
                 <input name="name" placeholder="Ex. Twitter Contest">
             </div>
             <div class="template-wrapper">
-                <h2>Choose a Template</h2>
-                <p class="text-prompt">Choose from one of our professionally designed templates or feel free to completely build your own!</p>
-                <div class="template-panels">
-                    <div class="panel default-panel">
-                        <div class="panel-header">
-                            Classic Raffle
-                        </div>
-                        <div class="panel-body">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        </div>
-                        <div class="panel-action" style="display: none;">
-                            <button class="btn select-btn"></button>
-                        </div>
-                    </div>
-                    <div class="panel blank">
-                        <div class="panel-header">
-                            Blank Canvas
-                        </div>
-                        <div class="panel-body">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        </div>
-                        <div class="panel-action" style="display: none;">
-                            <button class="btn select-btn"></button>
-                        </div>
-                    </div>
-                </div>
+                
             </div>
         </div>
         <div id="setup" class="tab-pane">

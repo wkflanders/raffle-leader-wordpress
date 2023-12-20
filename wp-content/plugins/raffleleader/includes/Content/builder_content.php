f19c40f9347d04c9830fdbcfa189b7ec56cabcec
wkflanders <wflanders@haverford.edu>
2023-12-20 07:47:28 -0500

    builder edits

--- wp-content/plugins/raffleleader/includes/Content/builder_content.php
+++ wp-content/plugins/raffleleader/includes/Content/builder_content.php
@@ -1,31 +1,88 @@
 <div class="raffleleader-container">
-    <nav class="navbar">
-        <div class="nav-logo">
-            <img class="text-logo" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/TEXT-LOGO.png">
+    <nav class="rl-navbar">
+        <div class="rl-nav-logo">
+            <img class="rl-text-logo" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/TEXT-LOGO.svg">
         </div>
-        <ul class="nav-tabs">
+        <ul class="rl-nav-tabs">
             <li class="active"><a class="noSelect" href="#templates">Templates</a></li>
             <li><a class="noSelect" href="#setup">Set Up</a></li>
             <li><a class="noSelect" href="#publish">Publish</a></li>
         </ul>
-        <ul class="nav-extras">
+        <ul class="rl-nav-extras">
             <li><a class="close" href="admin.php?page=raffleleader_plugin"></a></li>
         </ul>
     </nav>
-    <div class="tab-content">
-        <div id="templates" class="tab-pane active">
-            <div class="giveaway-name">
-                <label>Raffle Name</label>
-                <input name="name" placeholder="Ex. Twitter Contest">
-            </div>
-            <div class="template-wrapper">
-                
+    <div class="rl-tab-content">
+        <div id="templates" class="rl-tab-pane active">
+            <div class="rl-template-wrapper">
+                <div class="rl-basics grid-row">
+                    <div class="label grid-item">
+                        <h2>The Basics</h2>
+                        <p>Your license key provides access to the plugin's features.</p>
+                    </div>
+                    <div class="rl-box grid-item">
+                        <div class="grid-text">
+                            <h3>Blank Raffle</h3>
+                            <p>Your license key provides access to the plugin's features.</p>
+                        </div>
+                    </div>
+                    <div class="rl-box grid-item"></div>
+                    <div class="rl-box grid-item"></div>
+                </div>
+                <div class="rl-campaigns grid-row">
+                    <div class="label grid-item">
+                        <h2>Campaigns</h2>
+                        <p>Your license key provides access to the plugin's features.</p>
+                    </div>
+                    <div class="rl-box grid-item">
+                        <div class="grid-text">
+                            <h3>Pre-Launch</h3>
+                            <p>Your license key provides access to the plugin's features.</p>
+                        </div>
+                    </div>
+                    <div class="rl-box grid-item">
+                        <div class="grid-text">
+                            <h3>Email</h3>
+                            <p>Your license key provides access to the plugin's features.</p>
+                        </div>
+                    </div>
+                    <div class="rl-box grid-item">
+                        <div class="grid-text">
+                            <h3>Refer-a-Friend</h3>
+                            <p>Your license key provides access to the plugin's features.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="rl-sm grid-row">
+                    <div class="label grid-item">
+                        <h2>Social Media</h2>
+                        <p>Your license key provides access to the plugin's features.</p>
+                    </div>
+                    <div class="rl-box grid-item">
+                        <div class=grid-text>
+                            <h3>Instagram</h3>
+                            <p>Your license key provides access to the plugin's features.</p>
+                        </div>
+                    </div>
+                    <div class="rl-box grid-item">
+                        <div class="grid-text">
+                            <h3>TikTok</h3>
+                            <p>Your license key provides access to the plugin's features.</p>
+                        </div>
+                    </div>
+                    <div class="rl-box grid-item">
+                        <div class="grid-text">
+                            <h3>Twitter/X</h3>
+                            <p>Your license key provides access to the plugin's features.</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-        <div id="setup" class="tab-pane">
+        <div id="setup" class="rl-tab-pane">
             <h3>Setup</h3>
         </div>
-        <div id="publish" class="tab-pane">
+        <div id="publish" class="rl-tab-pane">
             <h3>Publish</h3>
         </div>
     </div>

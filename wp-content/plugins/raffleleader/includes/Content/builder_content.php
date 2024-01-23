08f09d347d5b09d665665ab939e87fa1ccf9c441
wkflanders <wflanders@haverford.edu>
2024-01-22 20:12:20 -0500

    fix nav styling

--- wp-content/plugins/raffleleader/includes/Content/builder_content.php
+++ wp-content/plugins/raffleleader/includes/Content/builder_content.php
@@ -180,7 +180,7 @@
                                 <div class="customize-settings-box">
                                     <p>Header Text Alignment</p>
                                     <div class="customize-settings-dropdown">
-                                        <div class="dropdown-display dropdown-align">
+                                        <div class="dropdown-display dropdown-inline-btns">
                                             <img class="inline-btn align-left" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/left-text-align.png">
                                             <img class="inline-btn align-center" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/center-text-align.png">
                                             <img class="inline-btn align-right" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/right-text-align.png">
@@ -225,12 +225,12 @@
                                 <div class="customize-settings-box">
                                     <p>Header Font Style </p>
                                     <div class="customize-settings-dropdown">
-                                        <div class="dropdown-display dropdown-align">
-                                            <img class="inline-btn bold" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/bold.png">
-                                            <img class="inline-btn italicize" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/italicize.png">
-                                            <img class="inline-btn underline" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/underline.png">
-                                            <img class="inline-btn strike" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/strikethrough.png">
-                                            <img class="inline-btn overline" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/overline.png">
+                                        <div class="dropdown-display dropdown-inline-btns">
+                                            <img class="inline-btn bold-btn" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/bold.png">
+                                            <img class="inline-btn italicize-btn" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/italicize.png">
+                                            <img class="inline-btn underline-btn" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/underline.png">
+                                            <img class="inline-btn strike-btn" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/strikethrough.png">
+                                            <img class="inline-btn overline-btn" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/overline.png">
                                         </div>
                                     </div>
                                 </div>

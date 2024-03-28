5d1e280322647f1bb3ff3c788468b2f7e79bd332
wkflanders <wflanders@haverford.edu>
2024-03-28 08:31:18 -0400

    template images

--- wp-content/plugins/raffleleader/includes/Content/templates_content.php
+++ wp-content/plugins/raffleleader/includes/Content/templates_content.php
@@ -2,7 +2,7 @@
     <div class="rl-template-wrapper">
         <div class="rl-basics rl-grid-row first-row">
             <div class="label grid-item">
-                <h2>The Basics</h2>
+                <h2>Wireframes</h2>
                 <p>Your license key provides access to the plugin's features.</p>
             </div>
             <div class="rl-box grid-item template-item">
@@ -14,104 +14,7 @@
                     </div>
                 </div>
                 <div class="rl-box-action-blur" style="display: none;"></div>
-                <div class="template-spacer">
-                </div>
-                <div class="template-footer">
-                    <p class="template-footer-content">Raffle Rules and Terms</p>
-                    <p class="template-footer-content template-rl_link" target="_blank" href="https://raffleleader.com">Try <img id="footer-logo" class="template-footer-text-logo" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/footer_text_logo.svg"> For Yourself!</p>
-                </div>
-            </div>
-        </div>
-        <div class="rl-campaigns rl-grid-row">
-            <div class="label grid-item">
-                <h2>Campaigns</h2>
-                <p>Your license key provides access to the plugin's features.</p>
-            </div>
-            <div class="rl-box grid-item template-item">
-                <div class="rl-box-action" style="display: none;">
-                    <h2>Pre-Launch Raffle</h2>
-                    <p>Your license key provides access to the plugin's features.</p>
-                    <div class="select-btn-wrapper">
-                        <button id="plaunchTemplate" class="select-template">Select</button>
-                    </div>
-                </div>
-                <div class="rl-box-action-blur" style="display: none;"></div>
-                <div class="template-header">Header</div>
-                <div class="template-subheader">Subheader</div>
-                <div class="template-image plaunch-image">Insert An Image Here</div>
-                <div class="template-body-text plaunch-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sed libero sagittis, pretium ipsum ut, rhoncus nisi.</div>
-                <div class="template-entry plaunch-entry">
-                    <input placeholder="email...">
-                    <button>&rarr;</button>
-                </div>
-                <div class="template-footer">
-                    <p class="template-footer-content">Raffle Rules and Terms</p>
-                    <p class="template-footer-content template-rl_link" target="_blank" href="https://raffleleader.com">Try <img id="footer-logo" class="template-footer-text-logo" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/footer_text_logo.svg"> For Yourself!</p>
-                </div>
-            </div>
-            <div class="rl-box grid-item template-item">
-                <div class="rl-box-action" style="display: none;">
-                    <h2>Refer-A-Friend Raffle</h2>
-                    <p>Your license key provides access to the plugin's features.</p>
-                    <div class="select-btn-wrapper">
-                        <button id="referRaffle" class="select-template">Select</button>
-                    </div>
-                </div>
-                <div class="rl-box-action-blur" style="display: none;"></div>
-                <div class="template-header">Header</div>
-                <div class="template-subheader">Subheader</div>
-                <div class="template-image img-image">Insert An Image Here</div>
-                <div class="template-body-text img-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sed libero sagittis, pretium ipsum ut, rhoncus nisi.</div>
-                <div class="template-entry img-entry">
-                    <input placeholder="email...">
-                    <button>&rarr;</button>
-                </div>
-                <div class="template-additional img-additional">
-                    <div class="template-additional-text img-additional-text">
-                        <h2>Refer A Friend</h2>
-                        <p>for an extra entry!</p>
-                    </div>
-                    <button>+1</button>
-                </div>
-                <div class="template-footer">
-                    <p class="template-footer-content">Raffle Rules and Terms</p>
-                    <p class="template-footer-content template-rl_link" target="_blank" href="https://raffleleader.com">Try <img id="footer-logo" class="template-footer-text-logo" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/footer_text_logo.svg"> For Yourself!</p>
-                </div>
-            </div>
-            <div class="rl-box grid-item template-item">
-                <div class="rl-box-action" style="display: none;">
-                    <h2>Image Entry Raffle</h2>
-                    <p>Your license key provides access to the plugin's features.</p>
-                    <div class="select-btn-wrapper">
-                        <button id="imageTemplate" class="select-template">Select</button>
-                    </div>
-                </div>
-                <div class="rl-box-action-blur" style="display: none;"></div>
-                <div class="template-header">Header</div>
-                <div class="template-subheader">Subheader</div>
-                <div class="template-image img-image">Insert An Image Here</div>
-                <div class="template-body-text img-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sed libero sagittis, pretium ipsum ut, rhoncus nisi.</div>
-                <div class="template-entry img-entry">
-                    <input placeholder="email...">
-                    <button>&rarr;</button>
-                </div>
-                <div class="template-additional img-additional">
-                    <div class="template-additional-text img-additional-text">
-                        <h2>Enter An Image</h2>
-                        <p>for an extra entry!</p>
-                    </div>
-                    <button>+1</button>
-                </div>
-                <div class="template-footer">
-                    <p class="template-footer-content">Raffle Rules and Terms</p>
-                    <p class="template-footer-content template-rl_link" target="_blank" href="https://raffleleader.com">Try <img id="footer-logo" class="template-footer-text-logo" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/footer_text_logo.svg"> For Yourself!</p>
-                </div>
-            </div>
-        </div>
-        <div class="rl-sm rl-grid-row">
-            <div class="label grid-item">
-                <h2>Social Media</h2>
-                <p>Your license key provides access to the plugin's features.</p>
+                <img class="blank-template-img" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/blank_template.svg">
             </div>
             <div class="rl-box grid-item template-item">
                 <div class="rl-box-action" style="display: none;">
@@ -122,39 +25,7 @@
                     </div>
                 </div>
                 <div class="rl-box-action-blur" style="display: none;"></div>
-                <div class="template-header">Header</div>
-                <div class="template-subheader">Subheader</div>
-                <div class="template-image">Insert An Image Here</div>
-                <div class="template-body-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sed libero sagittis, pretium ipsum ut, rhoncus nisi.</div>
-                <div class="template-entry">
-                    <input placeholder="email...">
-                    <button>&rarr;</button>
-                </div>
-                <div class="template-additional">
-                    <div class="template-additional-text">
-                        <h2>Follow us on X/Twitter</h2>
-                        <p>for an extra entry!</p>
-                    </div>
-                    <button>+1</button>
-                </div>
-                <div class="template-additional">
-                    <div class="template-additional-text">
-                        <h2>Follow us on X/Twitter</h2>
-                        <p>for an extra entry!</p>
-                    </div>
-                    <button>+1</button>
-                </div>
-                <div class="template-additional">
-                    <div class="template-additional-text">
-                        <h2>Follow us on X/Twitter</h2>
-                        <p>for an extra entry!</p>
-                    </div>
-                    <button>+1</button>
-                </div>
-                <div class="template-footer">
-                    <p class="template-footer-content">Raffle Rules and Terms</p>
-                    <p class="template-footer-content template-rl_link" target="_blank" href="https://raffleleader.com">Try <img id="footer-logo" class="template-footer-text-logo" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/footer_text_logo.svg"> For Yourself!</p>
-                </div>
+                <img class="twitter-template-img" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/twitter_template.svg">
             </div>
             <div class="rl-box grid-item template-item">
                 <div class="rl-box-action" style="display: none;">
@@ -165,39 +36,7 @@
                     </div>
                 </div>
                 <div class="rl-box-action-blur" style="display: none;"></div>
-                <div class="template-header">Header</div>
-                <div class="template-subheader">Subheader</div>
-                <div class="template-image">Insert An Image Here</div>
-                <div class="template-body-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sed libero sagittis, pretium ipsum ut, rhoncus nisi.</div>
-                <div class="template-entry">
-                    <input placeholder="email...">
-                    <button>&rarr;</button>
-                </div>
-                <div class="template-additional">
-                    <div class="template-additional-text">
-                        <h2>Follow us on Instagram</h2>
-                        <p>for an extra entry!</p>
-                    </div>
-                    <button>+1</button>
-                </div>
-                <div class="template-additional">
-                    <div class="template-additional-text">
-                        <h2>Leave a comment on Instagram</h2>
-                        <p>for an extra entry!</p>
-                    </div>
-                    <button>+1</button>
-                </div>
-                <div class="template-additional">
-                    <div class="template-additional-text">
-                        <h2>Like us on Instagram</h2>
-                        <p>for an extra entry!</p>
-                    </div>
-                    <button>+1</button>
-                </div>
-                <div class="template-footer">
-                    <p class="template-footer-content">Raffle Rules and Terms</p>
-                    <p class="template-footer-content template-rl_link" target="_blank" href="https://raffleleader.com">Try <img id="footer-logo" class="template-footer-text-logo" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/footer_text_logo.svg"> For Yourself!</p>
-                </div>
+                <img class="instagram-template-img" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/instagram_template.svg">
             </div>
             <div class="rl-box grid-item template-item">
                 <div class="rl-box-action" style="display: none;">
@@ -208,39 +47,35 @@
                     </div>
                 </div>
                 <div class="rl-box-action-blur" style="display: none;"></div>
-                <div class="template-header">Header</div>
-                <div class="template-subheader">Subheader</div>
-                <div class="template-image">Insert An Image Here</div>
-                <div class="template-body-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sed libero sagittis, pretium ipsum ut, rhoncus nisi.</div>
-                <div class="template-entry">
-                    <input placeholder="email...">
-                    <button>&rarr;</button>
-                </div>
-                <div class="template-additional">
-                    <div class="template-additional-text">
-                        <h2>Follow us on TikTok</h2>
-                        <p>for an extra entry!</p>
+                <img class="tiktok-template-img" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/tiktok_template.svg">
+            </div>
+            <div class="rl-box grid-item template-item">
+                <div class="rl-box-action" style="display: none;">
+                    <h2>Pre-Launch Raffle</h2>
+                    <p>Your license key provides access to the plugin's features.</p>
+                    <div class="select-btn-wrapper">
+                        <button id="plaunchTemplate" class="select-template">Select</button>
                     </div>
-                    <button>+1</button>
                 </div>
-                <div class="template-additional">
-                    <div class="template-additional-text">
-                        <h2>Like our video on TikTok</h2>
-                        <p>for an extra entry!</p>
+                <div class="rl-box-action-blur" style="display: none;"></div>
+                <img class="prelaunch-template-img" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/prelaunch_template.svg">
+            </div>
+            <div class="rl-box grid-item template-item">
+                <div class="rl-box-action" style="display: none;">
+                    <h2>Refer-A-Friend Raffle</h2>
+                    <p>Your license key provides access to the plugin's features.</p>
+                    <div class="select-btn-wrapper">
+                        <button id="referRaffle" class="select-template">Select</button>
                     </div>
-                    <button>+1</button>
                 </div>
-                <div class="template-additional">
-                    <div class="template-additional-text">
-                        <h2>View our video on TikTok</h2>
-                        <p>for an extra entry!</p>
-                    </div>
-                    <button>+1</button>
-                </div>
-                <div class="template-footer">
-                    <p class="template-footer-content">Raffle Rules and Terms</p>
-                    <p class="template-footer-content template-rl_link" target="_blank" href="https://raffleleader.com">Try <img id="footer-logo" class="template-footer-text-logo" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/footer_text_logo.svg"> For Yourself!</p>
-                </div>
+                <div class="rl-box-action-blur" style="display: none;"></div>
+                <img class="refer-a-friend-template-img" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/refer-a-friend_template.svg">
+            </div>
+        </div>
+        <div class="rl-campaigns rl-grid-row">
+            <div class="label grid-item">
+                <h2>Pre-Designed</h2>
+                <p>Your license key provides access to the plugin's features.</p>
             </div>
         </div>
     </div>

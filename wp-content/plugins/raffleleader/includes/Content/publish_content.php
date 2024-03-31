8c063fffb88d2c7412e702fd6e1e1523feea3c99
wkflanders <wflanders@haverford.edu>
2024-03-31 13:22:29 -0400

    publish page styling

--- wp-content/plugins/raffleleader/includes/Content/publish_content.php
+++ wp-content/plugins/raffleleader/includes/Content/publish_content.php
@@ -1,3 +1,30 @@
 <div id="publish" class="rl-tab-pane">
-    <h3>Publish</h3>
+    <div class="publish-wrapper">
+        <div class="publish-row-wrapper">
+            <div class="publish-row">
+                <div class="publish-header">
+                    <h1>Publish Options</h1>
+                </div>
+            </div>
+            <div class="publish-row">
+                <div class="publish-box raffle-post-box">
+                    <h2>Create A Post</h2>
+                    <p>This creates a new post for your raffle. Available as a block or a shortcode.</p>
+                </div>
+                <div class="publish-box raffle-page-box">
+                    <h2>Create A Landing Page</h2>
+                    <p>This creates a new page for your raffle. Available as a block or a shortcode.</p>
+                </div>
+                <div class="publish-box raffle-insert-box">
+                    <h2>Insert Raffle</h2>
+                    <p>Raffles are able to be inserted in existing posts and pages as both a block or a shortcode.</p>
+                </div>
+            </div>
+            <div class="publish-row">
+                <div class="publish-tutorial">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
 </div>

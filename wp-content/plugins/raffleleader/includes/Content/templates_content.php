9a116c9a02188d76879c9b263ce490f3e6de22ff
wkflanders <wflanders@haverford.edu>
2024-06-15 04:51:23 -0400

    remove template box action

--- wp-content/plugins/raffleleader/includes/Content/templates_content.php
+++ wp-content/plugins/raffleleader/includes/Content/templates_content.php
@@ -8,70 +8,22 @@
             <button class="scroll-grid-btn scroll-grid-btn-left"><i class="scroll-grid-arrow scroll-grid-left"></i></button>
             <button class="scroll-grid-btn scroll-grid-btn-right"><i class="scroll-grid-arrow scroll-grid-right"></i></button>
             <div class="rl-template-carousel">
-                <div class="rl-box grid-item template-item">
-                    <div class="rl-box-action" style="display: none;">
-                        <h2>Blank Raffle</h2>
-                        <p>Your license key provides access to the plugin's features.</p>
-                        <div class="select-btn-wrapper">
-                            <button id="blankTemplate" class="select-template">Select</button>
-                        </div>
-                    </div>
-                    <div class="rl-box-action-blur" style="display: none;"></div>
+                <div id="blankTemplate" class="rl-box grid-item template-item">
                     <img class="blank-template-img" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/blank_template.svg">
                 </div>
-                <div class="rl-box grid-item template-item">
-                    <div class="rl-box-action" style="display: none;">
-                        <h2>X/Twitter Raffle</h2>
-                        <p>Your license key provides access to the plugin's features.</p>
-                        <div class="select-btn-wrapper">
-                            <button id="twitterTemplate" class="select-template">Select</button>
-                        </div>
-                    </div>
-                    <div class="rl-box-action-blur" style="display: none;"></div>
+                <div id="twitterTemplate" class="rl-box grid-item template-item">
                     <img class="twitter-template-img" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/twitter_template.svg">
                 </div>
-                <div class="rl-box grid-item template-item">
-                    <div class="rl-box-action" style="display: none;">
-                        <h2>Instagram Raffle</h2>
-                        <p>Your license key provides access to the plugin's features.</p>
-                        <div class="select-btn-wrapper">
-                            <button id="instagramTemplate" class="select-template">Select</button>
-                        </div>
-                    </div>
-                    <div class="rl-box-action-blur" style="display: none;"></div>
+                <div id="instagramTemplate" class="rl-box grid-item template-item">
                     <img class="instagram-template-img" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/instagram_template.svg">
                 </div>
-                <div class="rl-box grid-item template-item">
-                    <div class="rl-box-action" style="display: none;">
-                        <h2>TikTok Raffle</h2>
-                        <p>Your license key provides access to the plugin's features.</p>
-                        <div class="select-btn-wrapper">
-                            <button id="tiktokTemplate" class="select-template">Select</button>
-                        </div>
-                    </div>
-                    <div class="rl-box-action-blur" style="display: none;"></div>
+                <div id="tiktokTemplate" class="rl-box grid-item template-item">
                     <img class="tiktok-template-img" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/tiktok_template.svg">
                 </div>
-                <div class="rl-box grid-item template-item">
-                    <div class="rl-box-action" style="display: none;">
-                        <h2>Pre-Launch Raffle</h2>
-                        <p>Your license key provides access to the plugin's features.</p>
-                        <div class="select-btn-wrapper">
-                            <button id="plaunchTemplate" class="select-template">Select</button>
-                        </div>
-                    </div>
-                    <div class="rl-box-action-blur" style="display: none;"></div>
+                <div id="plaunchTemplate" class="rl-box grid-item template-item">
                     <img class="prelaunch-template-img" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/prelaunch_template.svg">
                 </div>
-                <div class="rl-box grid-item template-item">
-                    <div class="rl-box-action" style="display: none;">
-                        <h2>Refer-A-Friend Raffle</h2>
-                        <p>Your license key provides access to the plugin's features.</p>
-                        <div class="select-btn-wrapper">
-                            <button id="referRaffle" class="select-template">Select</button>
-                        </div>
-                    </div>
-                    <div class="rl-box-action-blur" style="display: none;"></div>
+                <div id="referTemplate" class="rl-box grid-item template-item">
                     <img class="refer-a-friend-template-img" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/refer-a-friend_template.svg">
                 </div>
             </div>
@@ -85,69 +37,21 @@
             <button class="scroll-grid-btn scroll-grid-btn-right"><i class="scroll-grid-arrow scroll-grid-right"></i></button>
             <div class="rl-template-carousel">
                 <div class="rl-box grid-item template-item">
-                    <div class="rl-box-action" style="display: none;">
-                        <h2>Blank Raffle</h2>
-                        <p>Your license key provides access to the plugin's features.</p>
-                        <div class="select-btn-wrapper">
-                            <button id="blankTemplate" class="select-template">Select</button>
-                        </div>
-                    </div>
-                    <div class="rl-box-action-blur" style="display: none;"></div>
                     <img class="blank-template-img" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/blank_template.svg">
                 </div>
                 <div class="rl-box grid-item template-item">
-                    <div class="rl-box-action" style="display: none;">
-                        <h2>X/Twitter Raffle</h2>
-                        <p>Your license key provides access to the plugin's features.</p>
-                        <div class="select-btn-wrapper">
-                            <button id="twitterTemplate" class="select-template">Select</button>
-                        </div>
-                    </div>
-                    <div class="rl-box-action-blur" style="display: none;"></div>
                     <img class="twitter-template-img" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/twitter_template.svg">
                 </div>
                 <div class="rl-box grid-item template-item">
-                    <div class="rl-box-action" style="display: none;">
-                        <h2>Instagram Raffle</h2>
-                        <p>Your license key provides access to the plugin's features.</p>
-                        <div class="select-btn-wrapper">
-                            <button id="instagramTemplate" class="select-template">Select</button>
-                        </div>
-                    </div>
-                    <div class="rl-box-action-blur" style="display: none;"></div>
                     <img class="instagram-template-img" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/instagram_template.svg">
                 </div>
                 <div class="rl-box grid-item template-item">
-                    <div class="rl-box-action" style="display: none;">
-                        <h2>TikTok Raffle</h2>
-                        <p>Your license key provides access to the plugin's features.</p>
-                        <div class="select-btn-wrapper">
-                            <button id="tiktokTemplate" class="select-template">Select</button>
-                        </div>
-                    </div>
-                    <div class="rl-box-action-blur" style="display: none;"></div>
                     <img class="tiktok-template-img" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/tiktok_template.svg">
                 </div>
                 <div class="rl-box grid-item template-item">
-                    <div class="rl-box-action" style="display: none;">
-                        <h2>Pre-Launch Raffle</h2>
-                        <p>Your license key provides access to the plugin's features.</p>
-                        <div class="select-btn-wrapper">
-                            <button id="plaunchTemplate" class="select-template">Select</button>
-                        </div>
-                    </div>
-                    <div class="rl-box-action-blur" style="display: none;"></div>
                     <img class="prelaunch-template-img" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/prelaunch_template.svg">
                 </div>
                 <div class="rl-box grid-item template-item">
-                    <div class="rl-box-action" style="display: none;">
-                        <h2>Refer-A-Friend Raffle</h2>
-                        <p>Your license key provides access to the plugin's features.</p>
-                        <div class="select-btn-wrapper">
-                            <button id="referRaffle" class="select-template">Select</button>
-                        </div>
-                    </div>
-                    <div class="rl-box-action-blur" style="display: none;"></div>
                     <img class="refer-a-friend-template-img" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/refer-a-friend_template.svg">
                 </div>
             </div>

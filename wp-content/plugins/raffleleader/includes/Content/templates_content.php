4dad10818ec5b751af87c5846ea6c543c86d25dc
wkflanders <wflanders@haverford.edu>
2024-06-17 11:00:54 -0400

    pre designed templates

--- wp-content/plugins/raffleleader/includes/Content/templates_content.php
+++ wp-content/plugins/raffleleader/includes/Content/templates_content.php
@@ -36,23 +36,20 @@
             <button class="scroll-grid-btn scroll-grid-btn-left"><i class="scroll-grid-arrow scroll-grid-left"></i></button>
             <button class="scroll-grid-btn scroll-grid-btn-right"><i class="scroll-grid-arrow scroll-grid-right"></i></button>
             <div class="rl-template-carousel">
-                <div class="rl-box grid-item template-item">
-                    <img class="blank-template-img" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/blank_template.svg">
+                <div id="pdTemplateOne" class="rl-box grid-item template-item">
+                    <img class="pd-template-one-img" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/UGC_alo.svg">
                 </div>
-                <div class="rl-box grid-item template-item">
-                    <img class="twitter-template-img" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/twitter_template.svg">
+                <div id="pdTemplateTwo" class="rl-box grid-item template-item">
+                    <img class="pd-template-two-img" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/UGC_bonaroo.svg">
                 </div>
-                <div class="rl-box grid-item template-item">
-                    <img class="instagram-template-img" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/instagram_template.svg">
+                <div id="pdTemplateThree" class="rl-box grid-item template-item">
+                    <img class="pd-template-three-img" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/UGC_collection.svg">
                 </div>
-                <div class="rl-box grid-item template-item">
-                    <img class="tiktok-template-img" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/tiktok_template.svg">
+                <div id="pdTemplateFour" class="rl-box grid-item template-item">
+                    <img class="pd-template-four-img" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/UGC_nfl.svg">
                 </div>
-                <div class="rl-box grid-item template-item">
-                    <img class="prelaunch-template-img" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/prelaunch_template.svg">
-                </div>
-                <div class="rl-box grid-item template-item">
-                    <img class="refer-a-friend-template-img" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/refer-a-friend_template.svg">
+                <div id="pdTemplateFive" class="rl-box grid-item template-item">
+                    <img class="pd-template-five-img" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/UGC_gopro.svg">
                 </div>
             </div>
         </div>

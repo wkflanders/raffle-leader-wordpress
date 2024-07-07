62663039f481fd3c1cfdc81b51c8487a2dc19b60
wkflanders <wflanders@haverford.edu>
2024-07-07 15:38:30 -0400

    prelim license info page content

--- wp-content/plugins/raffleleader/includes/Content/license_info_content.php
+++ wp-content/plugins/raffleleader/includes/Content/license_info_content.php
@@ -1,3 +1,15 @@
+<?php
+$license_key = get_option( 'raffleleader_license_key' );
+?>
+<nav class="raffleleader-license-navbar">
+    <div class="raffleleader-license-logo-container">
+        <img class="raffleleader-license-logo" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/TEXT-LOGO.svg">
+    </div>
+</nav>
 <div class="wrap">
-    <p>This is a test license</p>
+    <form method="post" action="options.php">
+        <div class="wrap-form">
+            <p>Your license key: <?php echo $license_key; ?></p>
+        </div>
+    </form>
 </div>

a6882ef8732cb86bc74f7c6f3e7d4f5a2bb96ebf
wkflanders <wflanders@haverford.edu>
2024-07-07 15:26:36 -0400

    adjust license page content

--- wp-content/plugins/raffleleader/includes/Content/license_content.php
+++ wp-content/plugins/raffleleader/includes/Content/license_content.php
@@ -1,9 +1,16 @@
+<nav class="raffleleader-license-navbar">
+    <div class="raffleleader-license-logo-container">
+        <img class="raffleleader-license-logo" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/TEXT-LOGO.svg">
+    </div>
+</nav>
 <div class="wrap">
-        <form method="post" action="options.php">
+    <form method="post" action="options.php">
+        <div class="wrap-form">
             <?php
             settings_fields('raffleleader_license');
             do_settings_sections('raffleleader_license');
             submit_button('Submit License Key');
             ?>
-        </form>
-    </div>
+        </div>
+    </form>
+</div>

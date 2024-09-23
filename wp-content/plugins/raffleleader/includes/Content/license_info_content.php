e76afe371ec1ce02ab092a118c686d6be9e14540
wkflanders <wflanders@haverford.edu>
2024-09-22 23:18:03 -0400

    fix license key escaping

--- wp-content/plugins/raffleleader/includes/Content/license_info_content.php
+++ wp-content/plugins/raffleleader/includes/Content/license_info_content.php
@@ -3,13 +3,13 @@ $license_key = get_option( 'raffleleader_license_key' );
 ?>
 <nav class="raffleleader-license-navbar">
     <div class="raffleleader-license-logo-container">
-        <img class="raffleleader-license-logo" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/TEXT-LOGO.svg">
+        <img class="raffleleader-license-logo" src="<?php echo esc_url(plugin_dir_url(dirname(__FILE__, 2)) . './assets/images/TEXT-LOGO.svg'); ?>">
     </div>
 </nav>
 <div class="wrap">
     <form method="post" action="options.php">
         <div class="wrap-form">
-            <p>Your license key: <?php echo $license_key; ?></p>
+            <p>Your license key: <?php echo esc_html($license_key); ?></p>
         </div>
     </form>
 </div>

059a20e9e46e00cf538c6a71f53c34a7fba937df
wkflanders <wflanders@haverford.edu>
2024-09-22 23:18:25 -0400

    fix template image url escaping

--- wp-content/plugins/raffleleader/includes/Content/templates_content.php
+++ wp-content/plugins/raffleleader/includes/Content/templates_content.php
@@ -9,22 +9,22 @@
             <button class="scroll-grid-btn scroll-grid-btn-right"><i class="scroll-grid-arrow scroll-grid-right"></i></button>
             <div class="rl-template-carousel">
                 <div id="blankTemplate" class="rl-box grid-item template-item">
-                    <img class="blank-template-img" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/blank_template.svg">
+                    <img class="blank-template-img" src="<?php echo esc_url(plugin_dir_url(dirname(__FILE__, 2)) . './assets/images/blank_template.svg'); ?>">
                 </div>
                 <div id="twitterTemplate" class="rl-box grid-item template-item">
-                    <img class="twitter-template-img" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/twitter_template.svg">
+                    <img class="twitter-template-img" src="<?php echo esc_url(plugin_dir_url(dirname(__FILE__, 2)) . './assets/images/twitter_template.svg'); ?>">
                 </div>
                 <div id="instagramTemplate" class="rl-box grid-item template-item">
-                    <img class="instagram-template-img" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/instagram_template.svg">
+                    <img class="instagram-template-img" src="<?php echo esc_url(plugin_dir_url(dirname(__FILE__, 2)) . './assets/images/instagram_template.svg'); ?>">
                 </div>
                 <div id="tiktokTemplate" class="rl-box grid-item template-item">
-                    <img class="tiktok-template-img" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/tiktok_template.svg">
+                    <img class="tiktok-template-img" src="<?php echo esc_url(plugin_dir_url(dirname(__FILE__, 2)) . './assets/images/tiktok_template.svg'); ?>">
                 </div>
                 <div id="plaunchTemplate" class="rl-box grid-item template-item">
-                    <img class="prelaunch-template-img" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/prelaunch_template.svg">
+                    <img class="prelaunch-template-img" src="<?php echo esc_url(plugin_dir_url(dirname(__FILE__, 2)) . './assets/images/prelaunch_template.svg'); ?>">
                 </div>
                 <div id="referTemplate" class="rl-box grid-item template-item">
-                    <img class="refer-a-friend-template-img" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/refer-a-friend_template.svg">
+                    <img class="refer-a-friend-template-img" src="<?php echo esc_url(plugin_dir_url(dirname(__FILE__, 2)) . './assets/images/refer-a-friend_template.svg'); ?>">
                 </div>
             </div>
         </div>
@@ -37,19 +37,19 @@
             <button class="scroll-grid-btn scroll-grid-btn-right"><i class="scroll-grid-arrow scroll-grid-right"></i></button>
             <div class="rl-template-carousel">
                 <div id="pdTemplateOne" class="rl-box grid-item template-item">
-                    <img class="pd-template-one-img" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/UGC_alo.svg">
+                    <img class="pd-template-one-img" src="<?php echo esc_url(plugin_dir_url(dirname(__FILE__, 2)) . './assets/images/UGC_alo.svg'); ?>">
                 </div>
                 <div id="pdTemplateTwo" class="rl-box grid-item template-item">
-                    <img class="pd-template-two-img" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/UGC_bonnaroo.svg">
+                    <img class="pd-template-two-img" src="<?php echo esc_url(plugin_dir_url(dirname(__FILE__, 2)) . './assets/images/UGC_bonnaroo.svg'); ?>">
                 </div>
                 <div id="pdTemplateThree" class="rl-box grid-item template-item">
-                    <img class="pd-template-three-img" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/UGC_collection.svg">
+                    <img class="pd-template-three-img" src="<?php echo esc_url(plugin_dir_url(dirname(__FILE__, 2)) . './assets/images/UGC_collection.svg'); ?>">
                 </div>
                 <div id="pdTemplateFour" class="rl-box grid-item template-item">
-                    <img class="pd-template-four-img" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/UGC_nfl.svg">
+                    <img class="pd-template-four-img" src="<?php echo esc_url(plugin_dir_url(dirname(__FILE__, 2)) . './assets/images/UGC_nfl.svg'); ?>">
                 </div>
                 <div id="pdTemplateFive" class="rl-box grid-item template-item">
-                    <img class="pd-template-five-img" src="<?php echo plugin_dir_url(dirname(__FILE__, 2)) ?> ../../assets/images/UGC_gopro.svg">
+                    <img class="pd-template-five-img" src="<?php echo esc_url(plugin_dir_url(dirname(__FILE__, 2)) . './assets/images/UGC_gopro.svg'); ?>">
                 </div>
             </div>
         </div>
